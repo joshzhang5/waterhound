16d31831722445367eced3b248f661a993379303
jzhang761 <joshzhang@gatech.edu>
2016-09-23 18:36:39 -0400

    minor text fixes

--- src/fxapp/MainFXApplication.java
+++ src/fxapp/MainFXApplication.java
@@ -11,7 +11,7 @@ public class MainFXApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../view/WelcomeScreen.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Waterhound Clean Water Application");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
